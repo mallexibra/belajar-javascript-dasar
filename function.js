6eaba6736ca23796c89fe9f22c8e21c875eb3e8b
mallexibra <mallexibra@gmail.com>
2022-06-14 00:10:52 +0700

    Belajar function

--- function.js
+++ function.js
@@ -4,12 +4,38 @@
 // let volumeB= b**3;
 // let total = volumeA + volumeB;
 
-function jumlahVolume(a, b){
-	let volumeA = a**3;
-	let volumeB = b**3;
-	let total = volumeA + volumeB;
+// function jumlahVolume(a, b){
+// 	let volumeA = a**3;
+// 	let volumeB = b**3;
+// 	let total = volumeA + volumeB;
 
-	return total;
+// 	return total;
+// }
+
+// function tambah(a, b){
+// 	return a + b;
+// }
+
+// function kali(a, b){
+// 	return a * b;
+// }
+
+// let hasil = tambah(kali(2, 3), kali(5, 10));
+
+// Arguments
+function tambah(a, b){
+	
+	let coba = 0;
+
+	for(i = 0; i < arguments.length; i++){
+		coba += arguments[i];
+	}
+
+	coba += a + b;
+
+	return coba;
 }
 
-console.log(jumlahVolume(3, 5));
+let hasil = tambah(5,4,10);
+
+console.log(hasil);
